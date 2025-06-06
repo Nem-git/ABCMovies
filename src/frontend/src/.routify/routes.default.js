c82a570feda1829b2e8baf1350cd67e51df4f1bd
Nem-git <loukastjean06@gmail.com>
2025-06-06 02:04:52 -0400

    Recreated the project using only vite and Routify

---
+++ src/frontend/src/.routify/routes.default.js
@@ -0,0 +1,38 @@
+// @ts-nocheck
+
+
+export const routes = {
+  "meta": {},
+  "id": "_default",
+  "name": "",
+  "file": {
+    "path": "src/routes",
+    "dir": "src",
+    "base": "routes",
+    "ext": "",
+    "name": "routes"
+  },
+  "rootName": "default",
+  "routifyDir": import.meta.url,
+  "children": [
+    {
+      "meta": {
+        "dynamic": true,
+        "dynamicSpread": true,
+        "order": false,
+        "inline": false
+      },
+      "name": "[...404]",
+      "file": {
+        "path": ".routify/components/[...404].svelte",
+        "dir": ".routify/components",
+        "base": "[...404].svelte",
+        "ext": ".svelte",
+        "name": "[...404]"
+      },
+      "asyncModule": () => import('./components/[...404].svelte'),
+      "children": []
+    }
+  ]
+}
+export default routes
--- src/frontend/src/vite.config.ts
+++ src/frontend/src/vite.config.ts
@@ -1,6 +1,13 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import routify from '@roxi/routify/vite-plugin'
 
+// https://vite.dev/config/
 export default defineConfig({
-	plugins: [sveltekit()]
-});
+  plugins: [
+    svelte(),
+    routify({
+      
+    })
+  ],
+})
