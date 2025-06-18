ceb85a37a0b68319dd4e1b17bd46adcf1a789cc2
Nem-git <loukastjean06@gmail.com>
2025-06-18 09:58:50 -0400

    Svelte: Added prettier for file formatting in dev PHP: Added Codesniffer and CS-Fixer to format files

---
+++ clean.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+export PHP_CS_FIXER_IGNORE_ENV=true
+
+# PHP
+./src/backend/vendor/bin/phpcbf ./src/backend/config
+./src/backend/vendor/bin/phpcbf ./src/backend/public
+./src/backend/vendor/bin/phpcbf ./src/backend/src
+
+./src/backend/vendor/bin/php-cs-fixer fix ./src/backend/config
+./src/backend/vendor/bin/php-cs-fixer fix ./src/backend/public
+./src/backend/vendor/bin/php-cs-fixer fix ./src/backend/src
+
+# Python
+./src/python-backend/.venv/bin/black ./src/python-backend/main.py
+./src/python-backend/.venv/bin/black ./src/python-backend/app
+
+# Svelte
+npx prettier --write ./src/frontend/src/
