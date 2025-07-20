8ff6a9e3af17964655fca839810c8469eda88ccd
Nem-git <loukastjean06@gmail.com>
2025-07-19 22:28:33 -0400

    Docker: Set up both dev and prod docker environments, so that project can be more easily shared and deployed

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -4,8 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
-        port: 8000,
+        port: 8001,
         host: "0.0.0.0"
 	},
-	
 });
