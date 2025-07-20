3cfb706eaae9a7767a7bf7f6fdfb27700c41d946
Nem-git <loukastjean06@gmail.com>
2025-07-19 22:29:15 -0400

    PHP: Removed dotend, because environment variables are set directly in compose file

--- src/backend/public/index.php
+++ src/backend/public/index.php
@@ -5,15 +5,10 @@ declare(strict_types=1);
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
-use Dotenv\Dotenv;
 use App\Helpers\SlimResponseHelper;
 use App\Factory\ObjectFactory;
 
 require __DIR__ . "/../vendor/autoload.php";
-
-// Loads the environment variables from .env to $_ENV
-$dotenv = Dotenv::createImmutable(__DIR__ . "/../src/Config/");
-$dotenv->load();
 
 $app = AppFactory::create();
 
