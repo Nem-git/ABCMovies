c3d524d5511843048e3fb9f6d32be47631df7a83
Nem-git <loukastjean06@gmail.com>
2025-09-19 10:50:59 -0400

    Go: Added structs to define the responses of the API

--- src/go-backend/api/service.go
+++ src/go-backend/api/service.go
@@ -7,6 +7,15 @@ type StreamingServiceSlugs struct {
 
 // Streaming Service Response
 type StreamingServiceResponse struct {
-	// Streaming Service
-	//StreamingService
+	BackdropUrl        string   `json:"backdropUrl"`
+	Id                 string   `json:"id"`
+	Name               string   `json:"name"`
+	OriginalName       string   `json:"originalName"`
+	Overview           string   `json:"overview"`
+	PosterUrl          string   `json:"posterUrl"`
+	MediaTypes         []string `json:"mediaTypes"`
+	OriginalLanguage   string   `json:"originalLanguage"`
+	HomePage           string   `json:"homePage"`
+	OriginCountry      string   `json:"originCountry"`
+	AvailabilityStatus string   `json:"availabilityStatus"`
 }
